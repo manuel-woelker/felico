9c956dcf698cff8deec71cfba8179d73cb53d998
Manuel Woelker <github@manuel.woelker.org>
2024-10-12 05:57:02 +0200

    feat(server): error logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,9 @@
 use axum::body::{Body, Bytes};
 use axum::extract::{Path, Request};
 use axum::middleware::Next;
-use axum::response::{ErrorResponse, Response};
+use axum::response::{ErrorResponse, IntoResponse, Response};
 use axum::{http::StatusCode, middleware, routing::get, Json, Router};
+use felico_compiler::infra::result::{FelicoError, FelicoResult};
 use http::{HeaderName, HeaderValue};
 use http_body_util::{BodyExt, Limited};
 use log::{info, warn};
@@ -10,36 +11,25 @@ use serde::Serialize;
 use std::time::Duration;
 use tower_http::classify::ServerErrorsFailureClass;
 use tower_http::services::{ServeDir, ServeFile};
-use tower_http::trace::TraceLayer;
-use tracing::Span;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
     info!("Starting up;");
-    let serve_dir = ServeDir::new("./docui/dist/assets")
-        .not_found_service(ServeFile::new("./docui/dist/index.html"));
+    let serve_index_html = ServeFile::new("./docui/dist/index.html");
+    let serve_dir =
+        ServeDir::new("./docui/dist/assets").not_found_service(serve_index_html.clone());
+
     // build our application with a route
     let app = Router::new()
+        .nest_service("/", serve_index_html)
         .route("/foo", get(|| async { "Hi from /foo" }))
         .nest_service("/assets", serve_dir.clone())
         .fallback_service(serve_dir)
-        // `GET /` goes to `root`
-        //        .route("/", get(root))
-        // `POST /users` goes to `create_user`
         .route("/api/packages", get(get_packages))
         .route("/api/packages/:package_name/:package_version", get(get_package))
         .layer(middleware::from_fn(error_logging_middleware))
-        .layer(TraceLayer::new_for_http()
-            .on_response(|response: &Response<_>, _latency: Duration, _span: &Span| {
-                if !response.status().is_success() {
-                    warn!("HTTP Error {} for {:?}", response.status(), response)
-                }
-            })
-            .on_failure(|_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                tracing::error!("something went wrong: {:?}", _error);
-            }))
         // end
         ;
 
@@ -51,36 +41,56 @@ async fn main() {
 }
 
 async fn error_logging_middleware(req: Request, next: Next) -> Response {
+    let uri = req.uri().clone();
     let response = next.run(req).await;
-    if !response.status().is_success() {
-        warn!("HTTP Error {} for {:?}", response.status(), response);
-        let (mut parts, body) = response.into_parts();
-        let collect = Limited::new(body, 10000).collect().await.unwrap();
-        let bytes = collect.to_bytes();
-        warn!("Collected {:?}", bytes);
-        let bytes1 = "f00".as_bytes();
-        let len = bytes1.len();
-        let new_body = Body::from(bytes1);
-        parts.headers.remove(http::header::CONTENT_LENGTH);
-        parts
-            .headers
-            .insert(http::header::CONTENT_LENGTH, HeaderValue::from(len));
-        return Response::from_parts(parts, new_body);
+    if response.status().is_success() || response.status() == StatusCode::NOT_MODIFIED {
+        return response;
     }
+    let error_message = response
+        .extensions()
+        .get::<ErrorMessage>()
+        .map(|e| e.0.to_string())
+        .unwrap_or_else(|| "Unknown error".to_string());
+    warn!(
+        "HTTP Error {} for {}: {}",
+        response.status(),
+        uri,
+        error_message
+    );
     response
+}
+
+#[derive(Debug)]
+pub struct ServerError(FelicoError);
+
+#[derive(Debug, Clone)]
+pub struct ErrorMessage(String);
+
+impl<T: Into<FelicoError>> From<T> for ServerError {
+    fn from(value: T) -> Self {
+        Self(value.into())
+    }
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        let mut response = (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response();
+        response
+            .extensions_mut()
+            .insert(ErrorMessage(self.0.to_string()));
+        response
+    }
 }
 
 async fn get_package(
     Path((package_name, package_version)): Path<(String, String)>,
-) -> Result<Json<PackageDescription>, ErrorResponse> {
-    return Err(ErrorResponse::from(
-        Response::builder()
-            .status(500)
-            .body(Body::from("oh no!"))
-            .unwrap(),
-    ));
-    /*
-    Json(PackageDescription {
+) -> Result<Json<PackageDescription>, ServerError> {
+    //    Err("foo".into())
+    Ok(Json(PackageDescription {
         info: PackageInfo {
             name: package_name,
             version: package_version,
@@ -94,7 +104,7 @@ async fn get_package(
             name: "debug_print".to_string(),
             signature: "fun debug_print(item: any)".to_string(),
         }],
-    })*/
+    }))
 }
 
 async fn get_packages() -> (StatusCode, Json<PackageIndex>) {
