2d91ea3a367545f451e67bd62fd381077dab08f6
Manuel Woelker <manuel.woelker+jj@gmail.com>
2025-09-21 12:38:01 +0200

    fix(CI): remove backtraces from tests to make CI build again

--- base/src/error.rs
+++ base/src/error.rs
@@ -1,3 +1,4 @@
+use std::backtrace::{Backtrace, BacktraceStatus};
 use std::fmt::{Debug, Formatter};
 
 mod message_error;
@@ -7,19 +8,22 @@ pub use source_error::SourceError;
 
 pub struct FelicoError {
     pub error: Box<dyn std::error::Error>,
+    pub backtrace: Backtrace,
 }
 
 impl Debug for FelicoError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(&self.error, f)
+        writeln!(f, "Error: {}", self.error)?;
+        if self.backtrace.status() == BacktraceStatus::Captured {
+            writeln!(f, "{}", self.backtrace)?;
+        }
+        Ok(())
     }
 }
 
 impl FelicoError {
     pub fn message(s: impl Into<String>) -> Self {
-        Self {
-            error: Box::new(MessageError::from(s)),
-        }
+        MessageError::from(s).into()
     }
 }
 
@@ -30,6 +34,36 @@ where
     fn from(value: T) -> Self {
         Self {
             error: Box::new(value),
+            backtrace: Backtrace::capture(),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    /*
+       use expect_test::expect;
+       use crate::error::FelicoError;
+       use crate::result::FelicoResult;
+
+       #[inline(never)]
+       fn foo() -> FelicoResult<()> {
+           bar()
+       }
+
+       #[inline(never)]
+       fn bar() -> FelicoResult<()> {
+           Err(FelicoError::message("bar"))
+       }
+
+       #[test]
+       fn test_backtrace() {
+           let result = foo().expect_err("foo should fail");
+           expect!([r#"
+                Error: bar
+
+            "#]).assert_debug_eq(&result);
+       }
+
+    */
 }
